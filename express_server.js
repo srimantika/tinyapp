35853268f166e5d875613f7c4db72d83e7c68b56
srimantika <srimantika@gmail.com>
2021-11-08 19:11:10 +0000

    Added HTML response and also some comments to explain what each get is doing

--- express_server.js
+++ express_server.js
@@ -7,12 +7,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Sending a simple string "Hello!"
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Sending JSON string : Representing the entire urlDatabase object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+
+//Sending HTML : Response can contain HTML code, which would be rendered in the client browser
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
