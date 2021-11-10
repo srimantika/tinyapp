58ae0a33c954f0cc6770930b5fbf3f0277b6b587
srimantika <srimantika@gmail.com>
2021-11-10 03:19:29 +0000

    Added cookies logic and login and logout

--- express_server.js
+++ express_server.js
@@ -1,25 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser")
 const app = express();
 const PORT = 8080; // default port 8080
 
 // Setting ejs as the view engine
 app.set("view engine", "ejs");
 // Set the body-parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+
 //A new route handler for "/urls" with res.render() to pass the URL data to our template.
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 //A GET route to render the url submission form
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 // A GET route to match and handle the Post request of ShortURL form submission
@@ -33,16 +45,13 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 //A GET route for Login
-//app.get("/login", (req, res) => {
-//  if (cookieHasUser(req.session.user_id)) {
-//    res.redirect("/urls");
-//  } else {
-//    let templateVars = {
-//      user: users[req.session.user_id],
-//    };
-//    res.render("urls_login", templateVars);
-//  }
-//});
+app.get("/login", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };  
+  //res.render("urls_new", templateVars);
+});
 
 //Post Request receive form submission with randomly generated short url String
 app.post("/urls", (req, res) => {
@@ -62,6 +71,8 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+
+
 //Post Request for EDIT
 app.post("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
@@ -71,13 +82,18 @@ app.post("/urls/:id", (req, res) => {
 
 //Post Request for Login
 app.post("/login", (req, res) => {
-const user_id = req.body.user_id;
-res.cookie(user_id); 
-res.redirect("/urls");
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+//Post Request for Logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 //generates a Random String of 6 letters
-const generateRandomString = function() {
+const generateRandomString = function () {
   let randomString = "";
   for (let i = 0; i < 6; i++) {
     const randomCharCode = Math.floor(Math.random() * 26 + 97);
@@ -88,7 +104,7 @@ const generateRandomString = function() {
 };
 //Another route to add another page to display a single URL and its shortened form
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
